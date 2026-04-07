99bb1642e0501da9c81bdf3993116fe9e5865524
Practicum <kasymhr@gmail.com>
2026-04-07 18:12:53 +0500

    11 sprint commit

--- app/src/main/java/com/example/playlist_maker2/SearchViewHolder.kt
+++ app/src/main/java/com/example/playlist_maker2/SearchViewHolder.kt
@@ -9,6 +9,8 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val pictureMusic: ImageView = itemView.findViewById<ImageView>(R.id.music_picture)
@@ -16,11 +18,10 @@ class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val authorMusic: TextView = itemView.findViewById<TextView>(R.id.author_music)
     private val timeMusic: TextView = itemView.findViewById<TextView>(R.id.time_music)
 
-    @RequiresApi(Build.VERSION_CODES.S)
     fun bind(model: Track){
         nameMusic.text = model.trackName
         authorMusic.text = model.artistName
-        timeMusic.text = model.trackTime
+        timeMusic.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTimeMillis)
 
         Glide.with(itemView)
             .load(model.artworkUrl100)
--- app/src/main/java/com/example/playlist_maker2/Track.kt
+++ app/src/main/java/com/example/playlist_maker2/Track.kt
@@ -2,7 +2,7 @@ package com.example.playlist_maker2
 
 class Track(val trackName: String ,
             val artistName: String ,
-            val trackTime: String,
+            val trackTimeMillis: Long,
             val artworkUrl100: String) {
 
 }
